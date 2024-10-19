671a25b7f3e1ec843a047ecbce8f7209caae1a90
ankitsensei <main.ankitbhagat@gmail.com>
2024-10-19 23:46:59 +0530

    testing all files

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Hello Sir</h1>
+      <p>This is <u>Ankit</u></p>
+    </div>
+  )
+}
+
+export default App
