461f2f324b1ca9bafc3f9c9b1cdf6070fb423fda
ankitsensei <main.ankitbhagat@gmail.com>
2024-10-22 23:10:45 +0530

    using axios...data fetching

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import axios from 'axios'
 
-const App: React.FC = () => {
+const BASE_URL = "https://www.omdbapi.com/?i=tt3896198&apikey=2d5a1d48"
+
+const getMovieData = async () => {
+  try{
+    const response = await axios.get(BASE_URL);
+    console.log(response.data)
+  }
+  catch (error) {
+    console.log(error)
+  }
+}
+const App = () => {
+  useEffect(() => {
+    getMovieData()
+  },[])
   return (
-    <div>
-      <h1>Hello Sir</h1>
+    <div className="bg-zinc-900 w-full h-screen text-white">
+      <h1>Hello</h1>  
     </div>
   )
 }
 
-export default App  
+export default App
